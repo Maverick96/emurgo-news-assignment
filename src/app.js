63589d3eaa5611ae2590a1f54f09c34704795a92
Harsh Pancholi <harshpancholi8@gmail.com>
2024-08-31 18:08:47 +0530

    added swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerPath = path.join(__dirname, 'swagger.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
+
+
 const fetchArticles = require('./routes/fetchArticles');
 const findArticle = require('./routes/findArticle');
 const searchArticle = require('./routes/searchArticle');
@@ -10,6 +17,8 @@ const app = express();
 app.get('/fetchArticles', fetchArticles);
 app.get('/findArticle', findArticle);
 app.get('/searchArticle', searchArticle);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
