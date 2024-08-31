25311824acac825f28726dfc0720a6f0db66574e
Harsh Pancholi <harshpancholi8@gmail.com>
2024-08-31 17:52:52 +0530

    search article added

--- src/routes/findArticle.js
+++ src/routes/findArticle.js
@@ -2,30 +2,30 @@ const cache = require('../utils/cache');
 const gNewsService = require('../utils/gNews');
 
 async function findArticle(req, res) {
-    // the task had mentioned to search by title or author. but there are not apis to search based on author.
+    // the task had mentioned to search by title or author. but there is no api to search based on author.
     let { title } = req.query;
-    if(!title) 
-        return res.status(400).json({ error: 'Bad Request', message: 'title is a required field' });
+    if (!title)
+        return res.status(400).json({ error: 'Bad Request', message: 'title is a required param' });
 
     // stringifying so that search api get valid input
     title = JSON.stringify(title);
     const cacheKey = `title_${title}`;
     const cachedData = cache.get(cacheKey);
     // check if articles are already present in cache or not
-    if(cachedData) {
+    if (cachedData) {
         console.log("Serving data from cache");
         return res.json(cachedData);
     }
 
-    try{
+    try {
         const resp = await gNewsService.searchArticles(title, "title");
         // update cache with new articles
         cache.set(cacheKey, resp.data)
         return res.json(resp.data)
     }
-    catch(err) {
+    catch (err) {
         console.log("Error in getting articles from GNews", err);
-        return res.status(500).json({ error: "Internal Server Error", message: 'Error in fetching articles from Gnews'})
+        return res.status(500).json({ error: "Internal Server Error", message: 'Error in fetching articles from Gnews' })
     }
 }
 
--- src/routes/searchArticle.js
+++ src/routes/searchArticle.js
@@ -1,5 +1,31 @@
-function searchArticle(req, res) {
-    console.log("Search")
+const cache = require('../utils/cache');
+const gNewsService = require('../utils/gNews');
+
+async function searchArticle(req, res) {
+    let { searchBy } = req.query;
+    if(!searchBy) 
+        return res.status(400).json({ error: 'Bad Request', message: 'searchBy is a required param' });
+
+    searchBy = JSON.stringify(searchBy);
+    const cacheKey = `searchBy_${searchBy}`;
+    const cachedData = cache.get(cacheKey);
+    // check if articles are already present in cache or not
+    if(cachedData) {
+        console.log("Serving data from cache");
+        return res.json(cachedData);
+    }
+
+    try{
+        const resp = await gNewsService.searchArticles(searchBy);
+        // update cache with new articles
+        cache.set(cacheKey, resp.data)
+        return res.json(resp.data)
+    }
+    catch(err) {
+        console.log("Error in getting articles from GNews", err);
+        return res.status(500).json({ error: "Internal Server Error", message: 'Error in fetching articles from Gnews'})
+    }
+
 }
 
 module.exports = searchArticle;
